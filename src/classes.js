4f4b940dcc0eb7d48cc5065cf1667decbb44d291
Jeremiah Dulla <jeremiahdulla@gmail.com>
2024-08-20 19:05:40 -0400

    Add classes & buildTree function

---
+++ src/classes.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    this.root = buildTree(arr);
+  }
+}
+
+function buildTree(array) {
+  let sortedArr = [];
+  array
+    .sort((a, b) => a - b)
+    .forEach((element) => {
+      if (!sortedArr.includes(element)) {
+        sortedArr.push(element);
+      }
+    });
+
+  const build = function (arr) {
+    if (arr.length === 0) return null;
+    const mid = Math.floor(arr.length / 2);
+    const node = new Node(arr[mid]);
+    node.left = build(arr.slice(0, mid));
+    node.right = build(arr.slice(mid + 1));
+    return node;
+  };
+  return build(sortedArr);
+}
